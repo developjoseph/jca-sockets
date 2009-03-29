41a52c4da60f9a215068ba2076ca646eec8e25f7
mark.r.jeffrey <mark.r.jeffrey@b9046df2-04a8-11de-bd8f-71c2068a397f>
2009-03-29 19:31:48 +0000

    Add assembly Descriptors. These do not work yet.

--- b.bat
+++ b.bat
@@ -1,3 +1,24 @@
-call mvn clean package
-copy sockets-rar\target\sockets-rar.rar f:\tools\jboss-4.2.2.GA\server\default\deploy
-copy sockets-ejb\target\sockets-ejb.jar f:\tools\jboss-4.2.2.GA\server\default\deploy
+call mvn -Dmaven.test.skip=true clean package
+copy sockets-rar\target\sockets-rar.rar           f:\as\jboss-4.2.2.GA\server\default\deploy
+copy sockets-ejb\target\sockets-ejb-alpha.0.2.jar f:\as\jboss-4.2.2.GA\server\default\deploy
+
+copy sockets-rar\target\sockets-rar.rar           f:\as\jboss-4.0.3SP1\server\default\deploy
+copy sockets-ejb\target\sockets-ejb-alpha.0.2.jar f:\as\jboss-4.0.3SP1\server\default\deploy
+
+copy sockets-rar\target\sockets-rar.rar           f:\as\jboss-5.0.1.GA\server\default\deploy
+copy sockets-ejb\target\sockets-ejb-alpha.0.2.jar f:\as\jboss-5.0.1.GA\server\default\deploy
+
+
+copy sockets-rar\target\sockets-rar.rar           F:\as\glassfishv3-prelude\glassfish\domains\domain1\autodeploy
+copy sockets-ejb\target\sockets-ejb-alpha.0.2.jar F:\as\glassfishv3-prelude\glassfish\domains\domain1\autodeploy
+
+copy sockets-rar\target\sockets-rar.rar           F:\as\glassfish21\domains\domain1\autodeploy
+copy sockets-ejb\target\sockets-ejb-alpha.0.2.jar F:\as\glassfish21\domains\domain1\autodeploy
+
+copy sockets-rar\target\sockets-rar.rar           C:\post\apps\pod-v000606-ld-node1\deploy
+copy sockets-ejb\target\sockets-ejb-alpha.0.2.jar C:\post\apps\pod-v000606-ld-node1\deploy
+
+
+rem F:\as\glassfish21
+rem F:\as\bea\wlserver_10.3
+rem F:\as\Websphere2.1
